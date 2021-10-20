00c3cbba046fd80eafa62a2fe92176eab90add92
Alwyn Grant <marzgrant@gmail.com>
2021-10-19 23:07:54 -0700

    login form page template complete

--- Notflix/react-app/src/components/auth/LoginForm.js
+++ Notflix/react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './auth-styles/login.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +32,40 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <div className='login-container'>
+      <form className='login-form' onSubmit={onLogin}>
+        <div className='login-text'>Log in</div>
+        <div>
+          {errors.map((error, ind) => (
+            <div key={ind}>{error}</div>
+          ))}
+        </div>
+        <div className='login-email-div'>
+          <input
+            className='login-email-input'
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className='login-password-div'>
+          <input
+            className='login-password-input'
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
+        <div className='login-btn-div'>
+          <button className='login-btn' type='submit'>Login</button>
+          <div className='login-to-signup'>New to Notflix? <a href='/'>Sign up</a></div>
+        </div>
+      </form>
+    </div>
   );
 };
 
