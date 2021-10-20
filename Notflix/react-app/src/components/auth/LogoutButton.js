40e45bd6a85efaed7a40a3c7025de406a127dbe2
Alwyn Grant <marzgrant@gmail.com>
2021-10-20 07:05:55 -0700

    revised auth state file

--- Notflix/react-app/src/components/auth/LogoutButton.js
+++ Notflix/react-app/src/components/auth/LogoutButton.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from '../../store/session';
+import { useHistory } from 'react-router-dom'
 
 const LogoutButton = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const history = useDispatch();
+
   const onLogout = async (e) => {
     await dispatch(logout());
+    history.push('/')
   };
 
   return <button onClick={onLogout}>Logout</button>;
