d4d78de14c2f0b76094d8c692735d0913031891a
Alwyn Grant <marzgrant@gmail.com>
2021-10-25 00:40:42 -0700

    added all notflix text logos, thanks Andrew!!

--- Notflix/react-app/src/components/navbar/NavBarMovies.js
+++ Notflix/react-app/src/components/navbar/NavBarMovies.js
@@ -4,15 +4,16 @@ import { useSelector } from 'react-redux';
 import LogoutButton from '../auth/LogoutButton';
 import './navbar-styles/navbar_movies.css'
 
+import notflix_text_1000 from './images/notflix_text_1000.png'
+
 const NavBarMovies = () => {
     const { profile_id } = useParams();
-    const accountHolder = useSelector(state => state.session.user);
 
     return (
         <nav className='navbar-movies'>
             <div className='navbar-movies-container'>
                 <a className='movies-logo' href={`/profiles/${profile_id}/movies`}>
-                    NOTFLIX_LOGO
+                    <img className='movies-logo' src={notflix_text_1000} />
                 </a>
                 <a href={`/profiles/${profile_id}/my-list`}>My list</a>
                 <LogoutButton />
--- Notflix/react-app/src/components/navbar/NavBarProfile.js
+++ Notflix/react-app/src/components/navbar/NavBarProfile.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './navbar-styles/navbar_profile.css'
+import './navbar-styles/navbar_profile.css';
+
+import notflix_text_1000 from './images/notflix_text_1000.png'
 
 const NavBarProfile = () => {
     return (
         <nav className='navbar-prof'>
             <div className='navbar-prof-container'>
                 <NavLink className='navbar-prof-logo' to='/'>
-                    NOTFLIX_LOGO
+                    <img className='navbar-prof-logo' src={notflix_text_1000}/>
                 </NavLink>
             </div>
         </nav>
